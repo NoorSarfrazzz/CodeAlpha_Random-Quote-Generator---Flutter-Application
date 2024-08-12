a262e1f5162e77ebb370a854e268cc149feedf3c
faiza2710 <faizaafzal2710@gmail.com>
2024-08-12 12:41:28 +0500

    Update the fav screen

--- lib/favourite_screen.dart
+++ lib/favourite_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:share_plus/share_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
@@ -23,94 +23,114 @@ class FavouritesScreen extends StatelessWidget {
       backgroundColor: Color(0xFF242E49),
       body: quotes.isEmpty
           ? Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              height: 130,
-              width: 130,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(100),
-                color: Colors.white54,
-              ),
-              child: const Icon(Icons.favorite, color: Color(0xFF242E49), size: 100),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            'You don\'t have any favorites yet.',
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 17,
-              color: Colors.yellow.shade600,
-            ),
-          ),
-          const Text(
-            'Click some hearts.',
-            style: TextStyle(
-              fontSize: 15,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      )
-          : Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 5,vertical: 10),
-        child: MasonryGridView.count(
-          controller: _scrollController,
-          itemCount: quotes.length,
-          shrinkWrap: true,
-          mainAxisSpacing: 8,
-          crossAxisCount: 2,
-          crossAxisSpacing: 8,
-          itemBuilder: (context, index) {
-            return Card(
-              color: Color(0xFF141A30),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      quotes[index].quote,
-                      style: const TextStyle(fontSize: 16, color: Colors.white),
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Container(
+                    height: 130,
+                    width: 130,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Colors.white54,
                     ),
-                    const SizedBox(height: 8),
-                    Text(
-                      '- ${quotes[index].author}',
-                      style: const TextStyle(fontSize: 14, color: Colors.grey),
-                    ),
-                    const SizedBox(height: 8),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(
-                          icon: const Icon(FontAwesomeIcons.copy, color: Colors.white, size: 20),
-                          onPressed: () {
-                            _copy(context, quotes[index].quote);
-                          },
-                        ),
-                        IconButton(
-                          icon: provider.isExist(quotes[index])
-                              ? const Icon(Icons.favorite, color: Colors.yellow)
-                              : const Icon(Icons.favorite_outline, color: Colors.yellow),
-                          onPressed: () {
-                            provider.toggleFavorites(quotes[index]);
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
+                    child: const Icon(Icons.favorite,
+                        color: Color(0xFF242E49), size: 100),
+                  ),
                 ),
+                const SizedBox(height: 20),
+                Text(
+                  'You don\'t have any favorites yet.',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 17,
+                    color: Colors.yellow.shade600,
+                  ),
+                ),
+                const Text(
+                  'Click some hearts.',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            )
+          : Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+              child: MasonryGridView.count(
+                controller: _scrollController,
+                itemCount: quotes.length,
+                shrinkWrap: true,
+                mainAxisSpacing: 8,
+                crossAxisCount: 2,
+                crossAxisSpacing: 8,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Color(0xFF141A30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            quotes[index].quote,
+                            style: const TextStyle(
+                                fontSize: 16, color: Colors.white),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            '- ${quotes[index].author}',
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.grey),
+                          ),
+                          const SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                icon: const Icon(FontAwesomeIcons.copy,
+                                    color: Colors.white, size: 20),
+                                onPressed: () {
+                                  _copy(context, quotes[index].quote);
+                                },
+                              ),
+                              IconButton(
+                                icon: provider.isExist(quotes[index])
+                                    ? const Icon(Icons.favorite,
+                                        color: Colors.yellow)
+                                    : const Icon(Icons.favorite_outline,
+                                        color: Colors.yellow),
+                                onPressed: () {
+                                  provider.toggleFavorites(quotes[index]);
+                                },
+                              ),
+                              IconButton(
+                                icon: const Icon(
+                                  FontAwesomeIcons.share,
+                                  color: Colors.white,
+                                  size: 20,
+                                ),
+                                onPressed: () async {
+                                  //_showModalBottomSheet();
+                                  if (quotes[index].quote.isNotEmpty) {
+                                    await Share.share(
+                                        '${quotes[index].quote} \n~${quotes[index].author}');
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
               ),
-            );
-          },
-        ),
-      ),
+            ),
     );
   }
 
@@ -160,7 +180,8 @@ class FavoriteProvider extends ChangeNotifier {
 
   Future<void> _saveFavorites() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> quoteList = _favoriteQuotes.map((quote) => jsonEncode(quote.toJson())).toList();
+    List<String> quoteList =
+        _favoriteQuotes.map((quote) => jsonEncode(quote.toJson())).toList();
     print('Saving favorites: $quoteList');
     await prefs.setStringList('favorites', quoteList);
   }
@@ -170,7 +191,9 @@ class FavoriteProvider extends ChangeNotifier {
     List<String>? quoteList = prefs.getStringList('favorites');
     if (quoteList != null) {
       _favoriteQuotes.clear();
-      _favoriteQuotes.addAll(quoteList.map((quote) => QuoteModel.fromJson(jsonDecode(quote))).toList());
+      _favoriteQuotes.addAll(quoteList
+          .map((quote) => QuoteModel.fromJson(jsonDecode(quote)))
+          .toList());
       print('Loaded favorites: $quoteList');
       notifyListeners();
     } else {
